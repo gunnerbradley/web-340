3a03ad9a807a0ac809df3ec2706e78fc7f5f1c9e
Gunner Bradley <gunnerbradley.dev@gmail.com>
2021-08-15 17:38:51 -0500

    exercise-1.3

---
+++ week-1/bradley-exercise-1.3.js
@@ -0,0 +1,5 @@
+const url = require("url");
+const parsedURL = url.parse("https://www.example.com/profile?name=smith");
+console.log(parsedURL.protocol);
+console.log(parsedURL.host);
+console.log(parsedURL.query);
